0000ce84ba412aa111df6acf80dd87e01355dadc
luangirma <luangirma2@gmail.com>
2022-04-08 00:51:52 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,13 @@
 #include <stdio.h>                
-/*
+
+/**
  * main -Entry point
  *
  * Description: A c program that demonstrates how to print with put funciton
  *
  * Return: Always 0 (Success) 
  */
-int main(void)
-
-{
-
-	puts("\"Programming is like building a multilingual puzzle");
-
-		return (0);
-
+int main(void){
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
