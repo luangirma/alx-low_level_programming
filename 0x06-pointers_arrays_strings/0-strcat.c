3bb3b684fe267052fc474fe028671c5783a6f02a
luangirma <luangirma2@gmail.com>
2022-04-21 18:54:18 -0700

    tasks

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * *_strcat - concatenates two strings
+ * @dest: second string 
+ * @src: first string 
+ * Return: string
+ */
+char *_strcat(char *dest, char *src)
+{
+  int x, y;
+
+  for (x = 0; dest[x] != '\0'; x++)
+    ;
+  for (y = 0; src[y] != '\0'; y++)
+  {
+    dest[x] = src[y];
+    x++;
+  }
+  dest[x] = '\0';
+  return (dest);
+}
